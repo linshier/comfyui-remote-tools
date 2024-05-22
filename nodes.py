e3e47c6e5b376490b298a968f65084ab02037b32
linshier <linshier@gmail.com>
2024-05-22 22:24:46 +0800

    add nodes LoadBase64FromRemote

--- nodes.py
+++ nodes.py
@@ -1,41 +1,40 @@
 import json
 import time
 import random
+import uuid
 from copy import deepcopy
 import requests
+import websocket
 
-try: GID
-except NameError:
-	GID = ''.join(random.choice("abcdefghijklmnopqrstupvxyz") for x in range(8))
-	print(f"Remote Tools: Set session ID to '{GID}'")
+client_id = str(uuid.uuid4())
 
 def get_client_id():
-	global GID
-	return(f"remote-tools-{GID}")
+    global client_id
+    return(f"remote-tools-{client_id}")
 
 def get_new_job_id():
-	job_id = f"{get_client_id()}-{int(time.time()*1000)}"
-	time.sleep(0.1)
-	return job_id
+    job_id = f"{get_client_id()}-{int(time.time()*1000)}"
+    time.sleep(0.1)
+    return job_id
 
-def dispatch_to_remote(remote_url, prompt, job_id=f"{get_new_job_id()}"):
-	prompt = deepcopy(prompt)
-	data = {
-		"prompt": prompt,
-		"client_id": get_client_id(),
-		"extra_data": {
-			"job_id": job_id,
-		}
-	}
-	ar = requests.post(
-		f"{remote_url}/prompt",
-		data    = json.dumps(data),
-		headers = {"Content-Type": "application/json"},
-		timeout = 4,
-	)
-	ar.raise_for_status()
-	data = ar.json()
-	return data['prompt_id']
+def dispatch_to_remote(remote_address, prompt, job_id=f"{get_new_job_id()}"):
+    prompt = deepcopy(prompt)
+    data = {
+        "prompt": prompt,
+        "client_id": get_client_id(),
+        "extra_data": {
+            "job_id": job_id,
+        }
+    }
+    ar = requests.post(
+        f"http://{remote_address}/prompt",
+        data    = json.dumps(data),
+        headers = {"Content-Type": "application/json"},
+        timeout = 4,
+    )
+    ar.raise_for_status()
+    data = ar.json()
+    return data['prompt_id']
 
 class SendBase64ToRemote:
     CATEGORY = "remote-tools"
@@ -45,7 +44,7 @@ class SendBase64ToRemote:
         return {
             "required": {
                 "base64": ("STRING", {"default": ""}),
-                "remote_url": ("STRING", {"default": "http://127.0.0.1:8188"}),
+                "remote_address": ("STRING", {"default": "127.0.0.1:8188"}),
                 "remote_node_id": ("STRING", {"default": "1"}),
                 "remote_node_input": ("STRING", {"default": "base64Images"}),
                 "remote_prompt": ("STRING", {"multiline": True, "dynamicPrompts": False}),
@@ -54,13 +53,55 @@ class SendBase64ToRemote:
     RETURN_TYPES = ()
     OUTPUT_NODE = True
 
-    def send_base64_to_remote(self, base64, remote_url, remote_node_id, remote_node_input, remote_prompt):
+    def send_base64_to_remote(self, base64, remote_address, remote_node_id, remote_node_input, remote_prompt):
         prompt = json.loads(remote_prompt)
         if remote_node_id in prompt:
-            prompt[remote_node_id]['inputs'][remote_node_input] = base64
-        prompt_id = dispatch_to_remote(remote_url, prompt)
+            prompt[remote_node_id]['inputs'][remote_node_input] = base64 # encoded([base64,])
+        prompt_id = dispatch_to_remote(remote_address, prompt)
         return ()
+
+class LoadBase64FromRemote:
+    CATEGORY = "remote-tools"
+    FUNCTION = "load_base64_from_remote"
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "remote_address": ("STRING", {"default": "127.0.0.1:8188"}),
+                "remote_ui_output": ("STRING", {"default": "base64Images"}),
+                "remote_prompt": ("STRING", {"multiline": True, "dynamicPrompts": False}),
+            },
+        }
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("base64",)
+
+    def load_base64_from_remote(self, remote_address, remote_ui_output, remote_prompt):
+        ws = websocket.WebSocket()
+        ws.connect("ws://{}/ws?clientId={}".format(remote_address, get_client_id()))
+
+        prompt = json.loads(remote_prompt)
+        prompt_id = dispatch_to_remote(remote_address, prompt)
+
+        base64= ""
+        while True:
+            out = ws.recv()
+            if not isinstance(out, str):
+                continue
+            message = json.loads(out)
+            if message['type'] != 'executed':
+                continue
+            data = message['data']
+            if data['prompt_id'] == prompt_id:
+                result = data['output'][remote_ui_output]
+                if isinstance(result, list): # [base64,]
+                    base64 = json.JSONEncoder().encode(result)
+                else:
+                    base64 = result
+                break
+        return (base64,)
+
 
 NODE_CLASS_MAPPINGS = {
     "SendBase64ToRemote" : SendBase64ToRemote,
+    "LoadBase64FromRemote" : LoadBase64FromRemote,
 }
